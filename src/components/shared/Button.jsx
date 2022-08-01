ac2eef1de63e0f3e33b15c771fedb4ca5520d7f4
Scott Maitland <scott.maitland@verint.com>
2022-08-01 15:32:31 +0100

    init commit

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+function Button({ children, version, type, isDisabled }) {
+  return (
+    <button
+      className={`btn btn-${version}`}
+      type={type}
+      disabled={isDisabled}
+    >
+      {children}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  version: "primary",
+  type: "button",
+  isDisabled: false,
+}
+
+Button.propType = {
+  children: PropTypes.node.isRequired,
+  version: PropTypes.string,
+  type: PropTypes.string,
+  isDisabled: PropTypes.bool,
+}
+
+export default Button
